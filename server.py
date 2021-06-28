a89e85c17ec8fa43ca6f0eed3f5ddcd6ea7ef103
SabrinaKall <sabrina.kall@epfl.ch>
2021-06-28 23:30:16 +0200

    more data + add sort by option

--- server.py
+++ server.py
@@ -14,18 +14,26 @@ faculties = list(labs["faculty"].unique())
 faculties.append("ALL")
 app.layout = html.Div([
     html.Div([dcc.Dropdown(id='faculty-select', options=[{'label': i, 'value': i} for i in faculties], value='IC', style={'width': '140px'})]),
+    html.Div([dcc.Dropdown(id='sort-select', options=[{'label': i, 'value': i} for i in ["women", "total"]], value='women', style={'width': '140px'})]),
     dcc.Graph('bar-chart-graph', config={'displayModeBar': False})
 ])
 
 @app.callback(
     Output('bar-chart-graph', 'figure'),
-    [Input('faculty-select', 'value')]
+    [Input('faculty-select', 'value'), Input('sort-select', 'value')]
 )
-def update_graph(faculty):
+def update_graph(faculty, sort_type):
     if faculty == 'ALL':
         faculty_labs = labs
     else:
         faculty_labs = (labs[labs['faculty'] == faculty]).sort_values(by="number_women")
+
+    if sort_type == 'women': 
+        faculty_labs = faculty_labs.sort_values(by="number_women")
+    else:
+        faculty_labs["total"] = faculty_labs['number_women'] + faculty_labs['number_men']
+        faculty_labs = faculty_labs.sort_values(by='total')
+
     fig = go.Figure(
         data=[
             go.Bar(
